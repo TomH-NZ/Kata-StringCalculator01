bd91cc283cd2a01586d06776358ebf9e949de948
Tom Hatcher <tom.hatcher@myob.com>
2020-07-21 08:13:30 +1200

    Initial Project

---
+++ StringCalculatorKata01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCalculatorKata01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
